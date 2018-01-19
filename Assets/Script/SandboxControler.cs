6d4438cc1c237e7e54287997431af93d287a25bf
hientp <hientp@vng.com.vn>
2018-01-19 12:08:23 +0700

    Add SandboxController

---
+++ Assets/Script/SandboxControler.cs
@@ -0,0 +1,414 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+public class SandboxControler : MonoBehaviour {
+
+    private int HEIGHT_KINECT = 424;
+    private int WIDTH_KINECT = 512;
+    //static int MIN_DIMEN = 240;
+    readonly int SKIP_FRAMES_MIN_MAX = 30;
+    readonly int SKIP_FRAMES_MAPCOLOR = 10;
+    readonly int SKIP_FRAMES_MAPHEIGHT = 5;
+    readonly float NORMALIZE_RAW_DATA = 1000.0f;
+
+    float[,] data;
+    ushort[] DepthImage;
+    private const int epsilon = 2;
+
+    public int maxHeightMap = 100;
+    public float heightOffset = 0.03f;
+    private int checkToWriteFile = 200;
+    private int countFrameMinMax = 0;
+    private int countFrameMapColor = 0;
+    private int countFrameMapHeight = 0;
+    private float maxVal = 0;
+    private float minVal = 0;
+    private static int currentMax = 0;
+    private GameObject mRain;
+    private GameObject mWater;
+    private float mMinYPosWater = -5f;
+    private float mMaxYPosWater = 20f;
+    private static bool isRaining;
+    private const int DRAINAGE_INTERVAL = 20;
+    private static int mDrainageCount = 0;
+    private static int oldMax = 0;
+    private static int oldMin = 0;
+    private static bool isChanged = false;
+    private static List<TreeInstance> treeList = new List<TreeInstance>(0);
+    private static int placedTrees = 0;
+
+    private KinectManager manager;
+    private KinectInterop.SensorData sensorData;
+
+    private void initKinectController()
+    {
+        manager = KinectManager.Instance;
+        if (manager && manager.IsInitialized())
+        {
+            sensorData = manager.GetSensorData();
+        }
+    }
+    // Use this for initialization
+    void Start()
+    {
+        initKinectController();
+
+        //MIN_DIMEN = Math.Min(HEIGHT_KINECT, WIDTH_KINECT);
+        data = new float[HEIGHT_KINECT, WIDTH_KINECT];
+        /*mRain = this.gameObject.transform.GetChild(0).gameObject;
+        mWater = this.gameObject.transform.GetChild(1).gameObject;*/
+    }
+
+    private void handleWater()
+    {
+        //mWater.transform.Translate(Vector3.up);
+
+        /*if (mWater.transform.position.y > -5)
+        {
+            mWater.transform.Translate(-Vector3.up);
+        }*/
+
+        mDrainageCount = ++mDrainageCount % DRAINAGE_INTERVAL;
+        float currentWaterY = mWater.transform.position.y;
+
+        if (isRaining && currentWaterY < mMaxYPosWater)
+        {
+            if (mDrainageCount >= DRAINAGE_INTERVAL - 1)
+            {
+                mWater.transform.Translate(Vector3.up);
+            }
+        }
+        if (!isRaining && currentWaterY > mMinYPosWater)
+        {
+            if (mDrainageCount >= DRAINAGE_INTERVAL - 1)
+            {
+                mWater.transform.Translate(-Vector3.up);
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //handleWater();
+        DepthImage = (ushort[]) sensorData.depthImage.Clone();
+        /*checkToWriteFile--;
+        if (checkToWriteFile == 0)
+        {
+            using (FileStream fs = new FileStream("a.txt", FileMode.CreateNew, FileAccess.Write))
+            using (StreamWriter sw = new StreamWriter(fs))
+            {
+
+                for (int i = 0; i < 424 * 512; i++)
+                {
+                    sw.Write(sensorData.depthImage[i]);
+                    sw.Write(" ");
+
+                }
+                sw.Close();
+            }
+        }
+        */
+        loadDepthDataToTerrain(GetComponent<Terrain>().terrainData, DepthImage);
+       
+        
+    }
+    
+
+    void loadDepthDataToTerrain(TerrainData tData, ushort[] rawData)
+    {
+        int i = rawData.Length - 1;
+        if (countFrameMinMax == 0)
+        {
+            maxVal = (float)rawData.Max() / NORMALIZE_RAW_DATA;
+            minVal = (float)rawData.Min() / NORMALIZE_RAW_DATA;
+        }
+        countFrameMinMax = (countFrameMinMax + 1) % SKIP_FRAMES_MIN_MAX;
+
+        if (countFrameMapHeight == 0)
+        {
+            for (int y = 0; y < HEIGHT_KINECT; y++)
+            {
+                for (int x = 0; x < WIDTH_KINECT; x++)
+                {
+                    //flatten background
+                    if (rawData[i] >= minVal && rawData[i] <= minVal + heightOffset)
+                    {
+                        data[y, x] = minVal;
+                    } else
+                    {
+                        data[y,x] = maxVal - (float)rawData[i] / NORMALIZE_RAW_DATA;
+                    }
+                    //data[y, x] = (y + x) / 6000.0f;
+                    i--;
+                }
+                // to make terrain square
+                //i = i - 80;
+            }
+            tData.size = new Vector3(HEIGHT_KINECT, maxHeightMap, WIDTH_KINECT);
+            tData.SetHeights(0, 0, data);
+        }
+        countFrameMapHeight = (countFrameMapHeight + 1) % SKIP_FRAMES_MAPHEIGHT;
+
+        //WriteTerrainHeightMap(tData);
+        //if (checkToWriteFile)
+        //{
+        //    checkToWriteFile = false;
+        //    guestThresholdMapColor(tData);
+        //}
+
+        /*if (countFrameMapColor == 0)
+        {
+            KeyValuePair<int, int> threshold = guestThresholdMapColor(tData);
+            mapColor(tData, threshold);
+        }
+        countFrameMapColor = (countFrameMapColor + 1) % SKIP_FRAMES_MAPCOLOR;
+        */
+    }
+
+    private void mapColor(TerrainData terrainData, KeyValuePair<int, int> threshold)
+    {
+        int diffThreshold;
+
+
+        if (currentMax != 0)
+        {
+            if (threshold.Value - currentMax <= epsilon)
+            {
+                currentMax = threshold.Value;
+
+                //mRain.SetActive(false);
+                isRaining = false;
+            }
+            else
+            {
+                //mRain.SetActive(true);
+                isRaining = true;
+            }
+            diffThreshold = currentMax - threshold.Key;
+        }
+        else
+        {
+            diffThreshold = threshold.Value - threshold.Key;
+            currentMax = threshold.Value;
+        }
+        Debug.Log("min: " + threshold.Key + "max: " + threshold.Value);
+
+        if (oldMax != threshold.Value || oldMin != threshold.Key)
+        {
+            isChanged = true;
+            oldMax = threshold.Value;
+            oldMin = threshold.Key;
+            if (treeList != null && treeList.Count != 0)
+            {
+                deleteAllTree();
+            }
+        }
+        else
+        {
+            isChanged = false;
+        }
+
+        float[,,] splatmapData = new float[terrainData.alphamapWidth, terrainData.alphamapHeight, terrainData.alphamapLayers];
+
+        for (int y = 0; y < terrainData.alphamapHeight; y++)
+        {
+            for (int x = 0; x < terrainData.alphamapWidth; x++)
+            {
+                // Normalise x/y coordinates to range 0-1 
+                float y_01 = (float)y / (float)terrainData.alphamapHeight;
+                float x_01 = (float)x / (float)terrainData.alphamapWidth;
+
+                float height = terrainData.GetHeight(y, x);
+
+                // Setup an array to record the mix of texture weights at this point
+                float[] splatWeights = new float[terrainData.alphamapLayers];
+
+                handleSetWeights(height, diffThreshold, 5, splatWeights, placedTrees, treeList, x, y, terrainData.heightmapHeight, terrainData.heightmapWidth);
+
+                // Sum of all textures weights must add to 1, so calculate normalization factor from sum of weights
+                float z = splatWeights.Sum();
+
+                // Loop through each terrain texture
+                for (int i = 0; i < terrainData.alphamapLayers; i++)
+                {
+                    // Normalize so that sum of all texture weights = 1
+                    splatWeights[i] /= z;
+
+                    // Assign this point to the splatmap array
+                    splatmapData[x, y, i] = splatWeights[i];
+                }
+            }
+        }
+
+        // Finally assign the new splatmap to the terrainData:
+        terrainData.SetAlphamaps(0, 0, splatmapData);
+        terrainData.treeInstances = treeList.ToArray();
+        terrainData.SetHeights(0, 0, new float[,] { { } });
+    }
+
+    private float[] handleSetWeights(double height, double diffThreshold, int nLayers, float[] splatWeights, int placedTrees, List<TreeInstance> treeList, int x, int y, int mapHeight, int mapWidth)
+    {
+        double a = height * 1.0f / (diffThreshold / 5.0f);
+
+        for (int i = nLayers - 1; i >= 0; i--)
+        {
+            if (a >= i * 1.0f)
+            {
+                if (i != 0)
+                {
+                    double tmp = a - i * 1.0f;
+                    splatWeights[i] = (float)tmp;
+                    splatWeights[i - 1] = (float)(1.0 - tmp);
+                    if (i == 2 && splatWeights[i] > 0.89 && isChanged)
+                    {
+                        addTree(placedTrees, treeList, x, y, mapHeight, mapWidth);
+                    }
+                }
+                else
+                {
+                    splatWeights[i] = 1.0f;
+                }
+                break;
+            }
+        }
+        return splatWeights;
+    }
+
+    void WriteTerrainHeightMap(TerrainData terrainData)
+    {
+        bool checkToWriteFile = false;
+        if (checkToWriteFile)
+        {
+            checkToWriteFile = false;
+            using (FileStream fs = new FileStream("a.txt", FileMode.CreateNew, FileAccess.Write))
+            using (StreamWriter sw = new StreamWriter(fs))
+            {
+
+                for (int i = 0; i < terrainData.heightmapHeight; i++)
+                {
+                    for (int j = 0; j < terrainData.heightmapWidth; j++)
+                    {
+                        sw.Write(terrainData.GetHeight(i, j));
+                        sw.Write(" ");
+                    }
+                    sw.WriteLine();
+                }
+                sw.Close();
+            }
+        }
+    }
+
+    private KeyValuePair<int, int> guestThresholdMapColor(TerrainData tData)
+    {
+        int[] mappingVal = new int[200];
+        int min = 0, max = 0;
+
+        for (int i = 0; i < tData.heightmapHeight; i++)
+        {
+            for (int j = 0; j < tData.heightmapWidth; j++)
+            {
+                float tmp = tData.GetHeight(i, j);
+                mappingVal[(int)tmp]++;
+            }
+        }
+
+        for (int i = 1; i < 100; i++)
+        {
+            if (mappingVal[i] != 0)
+            {
+                min = i;
+                break;
+            }
+        }
+        for (int i = 99; i > 0; i--)
+        {
+            if (mappingVal[i] != 0)
+            {
+                max = i;
+                break;
+            }
+        }
+        //using (FileStream fs = new FileStream("a.txt", FileMode.CreateNew, FileAccess.Write))
+        //using (StreamWriter sw = new StreamWriter(fs))
+        //{
+
+        //    for (int i = 0; i < mappingVal.Length; i++)
+        //    {
+        //            sw.WriteLine(i + "   " + mappingVal[i]);   
+        //    }
+        //    sw.Close();
+        //}
+        return new KeyValuePair<int, int>(min, max);
+    }
+
+    private void deleteAllTree()
+    {
+        List<TreeInstance> newTrees = new List<TreeInstance>(0);
+        placedTrees = 0;
+        GetComponent<Terrain>().terrainData.treeInstances = newTrees.ToArray();
+    }
+
+    private void renderTree(TerrainData terrainData)
+    {
+        List<TreeInstance> treeList = new List<TreeInstance>(0);
+
+        int placedTrees = 0;
+
+        for (int i = 0; i < terrainData.heightmapHeight / 10; i++)
+        {
+            for (int j = 0; j < terrainData.heightmapWidth / 10; j++)
+            {
+                float percent = UnityEngine.Random.value;
+                if (percent > 0.99f)
+                {
+                    placedTrees++;
+
+                    //Vector3 treePos = new Vector3(0.0f + placedTrees / terrainData.heightmapWidth, 0.0f, 0.0f + placedTrees / terrainData.heightmapHeight);
+                    Vector3 treePos = new Vector3(percent, 0.0f, percent);
+                    TreeInstance tree = new TreeInstance();
+
+                    tree.position = treePos;
+                    tree.prototypeIndex = 0;
+                    tree.color = new UnityEngine.Color(1, 1, 1);
+                    tree.lightmapColor = new UnityEngine.Color(1, 1, 1);
+                    tree.heightScale = 1;
+                    tree.widthScale = 1;
+
+                    treeList.Add(tree);
+                }
+            }
+        }
+        //run after the loop
+        terrainData.treeInstances = treeList.ToArray();
+        terrainData.SetHeights(0, 0, new float[,] { { } });
+    }
+
+    private List<TreeInstance> addTree(int placedTrees, List<TreeInstance> treeList, int x, int y, int mapHeight, int mapWidth)
+    {
+        float percent = UnityEngine.Random.value;
+        if (percent > 0.99f)
+        {
+            placedTrees++;
+
+            Vector3 treePos = new Vector3(y * 1.0f / mapHeight, 0.0f, x * 1.0f / mapWidth);
+            TreeInstance tree = new TreeInstance();
+
+            tree.position = treePos;
+            tree.prototypeIndex = 0;
+            tree.color = new UnityEngine.Color(1, 1, 1);
+            tree.lightmapColor = new UnityEngine.Color(1, 1, 1);
+            tree.heightScale = 1;
+            tree.widthScale = 1;
+
+            treeList.Add(tree);
+        }
+
+        return treeList;
+    }
+}
